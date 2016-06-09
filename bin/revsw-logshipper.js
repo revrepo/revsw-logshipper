aea90b0763f25cf22ab6f98ee11870ccf867adb6
DmitryM <dmitry@mainlywrenches.co>
2016-06-09 20:46:41 +0400

    log db cleaning service implemented

--- bin/revsw-logshipper.js
+++ bin/revsw-logshipper.js
@@ -21,78 +21,142 @@
 
 //  ----------------------------------------------------------------------------------------------//
 
-var Hapi = require('hapi'),
-  // Swagger = require('hapi-swagger'),
-  fs = require('fs'),
-  config = require( 'config'),
+var Hapi = require( 'hapi' ),
+  fs = require( 'fs' ),
+  config = require( 'config' ),
   logger = require( 'revsw-logger' )( config.log ),
-  pkg = require( '../package.json' );
+  cluster = require( 'cluster' );
+
+var stuff = require( '../lib/commons' );
 
 
-//  log shipper ----------------------------------------------------------------------------------//
+//  init cluster ---------------------------------------------------------------------------------//
 
+if ( cluster.isMaster ) {
 
-if ( config.run_logshipping_jobs === true ) {
+  //  ---------------------------------
+  //  main cluster process setup
 
-  var mkdirp = require('mkdirp');
-  var stuff = require( '../lib/commons');
-  //  create `uploads` directory
-  mkdirp.sync( stuff.toUploadsPath( '' ) );
+  logger.info( 'Master pid ' + process.pid );
 
-  var logshipper = require( '../lib/dispatcher');
-  logger.info( 'Starting Log Shipping Service' );
-  logshipper.run();
-  setInterval( function() {
-    logshipper.run();
-  }, ( config.logs_shipping_span_sec * 1000 ) );
+  //  run workers
+  if ( config.run_logshipping_jobs === true ) {
+
+    //  create `uploads` directory
+    var mkdirp = require( 'mkdirp' );
+    mkdirp.sync( stuff.toUploadsPath( '' ) );
+
+    var logshipping_worker = cluster.fork( { worker_name: 'shipping' } );
+  } else {
+    logger.info( 'Log Shipping Service is disabled per configuration' );
+  }
+
+  if ( config.run_logcleaning_jobs === true ) {
+    var logscleaning_worker = cluster.fork( { worker_name: 'cleaning' } );
+  } else {
+    logger.info( 'Log Cleaning Service is disabled per configuration' );
+  }
+
+  cluster.on( 'exit', function( worker, code, signal ) {
+    if ( worker === logshipping_worker ) {
+      logger.warn( 'logshipping worker(' + worker.process.pid + ') died, respawning' );
+      logshipping_worker = cluster.fork( { worker_name: 'shipping' } );
+    } else if ( worker === logscleaning_worker ) {
+      logger.warn( 'logscleaning worker(' + worker.process.pid + ') died, respawning' );
+      logscleaning_worker = cluster.fork( { worker_name: 'cleaning' } );
+    }
+  } );
+
+  //  run simple api server in the main process
+  var server = new Hapi.Server();
+
+  server.connection( {
+    host: config.get( 'service.host' ),
+    port: config.get( 'service.https_port' ),
+    tls: {
+      key: fs.readFileSync( config.get( 'service.key_path' ) ),
+      cert: fs.readFileSync( config.get( 'service.cert_path' ) )
+    },
+    routes: { cors: true }
+  } );
+
+  server.connection( {
+    host: config.get( 'service.host' ),
+    port: config.get( 'service.http_port' )
+  } );
+
+  //  redirect all non-HTTPS requests to HTTPS
+  server.ext( 'onRequest', function( request, reply ) {
+    var https_port = config.get( 'service.https_port' );
+    if ( request.connection.info.port !== https_port ) {
+      return reply.redirect( 'https://' + request.info.hostname +
+        ( https_port !== 443 ? ( ':' + https_port ) : '' ) +
+        request.path ).code( 301 );
+    }
+    reply.continue();
+  } );
+
+  server.register( {
+    register: require( 'hapi-router' ),
+    options: {
+      routes: 'routes/*.js'
+    }
+  }, function( err ) {
+    if ( err ) {
+      throw err;
+    }
+  } );
+
+  //  ---------------------------------
+  server.start( function() {
+    logger.info( 'hapi server started, ' + server.info.uri );
+  } );
 
 } else {
-  logger.info( 'Log Shipping Service is disabled per configuration' );
+
+  // console.dir( process.env.worker_name );
+
+  //  ---------------------------------
+  //  worker processes setup
+
+  var dispatcher = require( '../lib/dispatcher' );
+
+  if ( process.env.worker_name === 'shipping' ) {
+
+    logger.info( 'logs shipping worker started, process id ' + process.pid );
+
+    dispatcher.runSipping();
+    setInterval( function() {
+      dispatcher.runSipping();
+    }, ( config.logs_shipping_span_sec * 1000 ) );
+
+  } else if ( process.env.worker_name === 'cleaning' ) {
+
+    logger.info( 'logs cleaning worker started, process id ' + process.pid );
+
+    setTimeout(function() {
+
+      setInterval( function() {
+        dispatcher.runCleaning();
+      }, ( config.logs_clearing_span_sec * 1000 ) );
+
+    }, config.logs_shipping_span_sec * 500/*shift on half of logshipping interval*/ );
+
+
+  }
+
 }
 
-//  simple api server ----------------------------------------------------------------------------//
 
-var server = new Hapi.Server();
 
-server.connection({
-  host: config.get('service.host'),
-  port: config.get('service.https_port'),
-  tls: {
-    key: fs.readFileSync(config.get('service.key_path')),
-    cert: fs.readFileSync(config.get('service.cert_path'))
-  },
-  routes: { cors: true }
-});
 
-server.connection({
-  host: config.get('service.host'),
-  port: config.get('service.http_port')
-});
 
-//  redirect all non-HTTPS requests to HTTPS
-server.ext('onRequest', function (request, reply) {
-  var https_port = config.get('service.https_port');
-  if ( request.connection.info.port !== https_port ) {
-    return reply.redirect( 'https://' + request.info.hostname +
-      ( https_port !== 443 ? ( ':' + https_port ) : '' ) +
-      request.path ).code( 301 );
-  }
-  reply.continue();
-});
 
-server.register({
-  register: require('hapi-router'),
-  options: {
-    routes: 'routes/*.js'
-  }
-}, function (err) {
-  if (err) {
-    throw err;
-  }
-});
 
-//  ---------------------------------
-server.start(function() {
-  logger.info( 'hapi server started, ' + server.info.uri );
-});
+
+
+
+
+
+//  ----------------------------------------------------------------------------------------------//
 
