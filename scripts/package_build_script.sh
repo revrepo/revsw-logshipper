14a869e7893516742bf7f2d51f409c39e6f2cc2e
Victor Gartvich <victor@revsw.com>
2016-06-30 19:15:08 +0000

    Fixed the packaging script to also copy models/ directory

--- scripts/package_build_script.sh
+++ scripts/package_build_script.sh
@@ -76,6 +76,7 @@ cp -rf  $WORKSPACE/bin  $foldername/opt/$PackageName/
 cp -rf  $WORKSPACE/lib  $foldername/opt/$PackageName/
 cp -rf  $WORKSPACE/handlers  $foldername/opt/$PackageName/
 cp -rf  $WORKSPACE/routes  $foldername/opt/$PackageName/
+cp -rf  $WORKSPACE/models  $foldername/opt/$PackageName/
 cp -rf  $WORKSPACE/node_modules  $foldername/opt/$PackageName/
 cp -rf  $WORKSPACE/config/*.json  $foldername/opt/$PackageName/config/
 cp -rf  $WORKSPACE/config/dev_ssl_certs  $foldername/opt/$PackageName/config/
