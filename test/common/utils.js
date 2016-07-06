de768ac885fee97ebf217e29030c7b8141a24c46
Yegor Dia <TheDialord@github.com>
2016-07-06 03:38:09 +0100

    ftp server binding fix

---
+++ test/common/utils.js
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2016] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+
+'use strict';
+
+var os = require('os');
+
+module.exports = {
+    getLocalIP: function() {
+        var ifaces = os.networkInterfaces();
+        Object.keys(ifaces).forEach(function (ifname) {
+            var alias = 0;
+
+            ifaces[ifname].forEach(function (iface) {
+                if ('IPv4' !== iface.family || iface.internal !== false) {
+                    return;
+                }
+
+                // if (alias >= 1) {
+                //     console.log(ifname + ':' + alias, iface.address);
+                // } else {
+                //     console.log(ifname, iface.address);
+                // }
+                if (iface.address.indexOf('192.168') !== -1) {
+                    return iface.address;
+                }
+                ++alias;
+            });
+        });
+        
+        return '127.0.0.1';
+    }
+};
